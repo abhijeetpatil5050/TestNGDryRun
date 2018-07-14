7432e3a5795846e9927d003f1d337e1a7e78cf14
Shree <Shree@Shree-PC>
2018-07-14 08:23:48 +0530

    Dry-Run ...

---
+++ TestNGTest/src/test/java/com/abp/TestNGTest/CheckAnno.java
@@ -0,0 +1,12 @@
+package com.abp.TestNGTest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class CheckAnno {
+
+	public void m1(){
+		System.out.println("inside method m1");
+	}
+	
+}
---
+++ TestNGTest/src/test/java/com/abp/TestNGTest/CheckAnno2.java
@@ -0,0 +1,12 @@
+package com.abp.TestNGTest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class CheckAnno2 {
+
+	public void m2(){
+		System.out.println("inside method m2");
+	}
+	
+}
